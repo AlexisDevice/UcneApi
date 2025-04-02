48d0b41a16d4d1457530316b0dd9a9b79277b35a
Alexis <AlexisNoMain@icloud.com>
2025-04-02 16:30:49 -0400

    Actualizando un endpoit que enviaba un arreglo sin necesidad

--- controllers/usuarios.py
+++ controllers/usuarios.py
@@ -10,14 +10,16 @@ def obtener_usuarios():
 
 def obtener_usuario(id):
     usuario = run_query(f"select * from Usuarios where id = {id}", True)
-    return [{"id": u[0], "nombre": u[1], "email": u[2], "matricula": u[3]} for u in usuario]
+    u = usuario[0]
+    return {"id": u[0], "nombre": u[1], "email": u[2], "matricula": u[3]}
 
 def login_usuario(email, password):
     usuario = run_query(f"select * from Usuarios where email = '{email}';", True)
+    usuario = usuario[0]
 
-    if usuario and usuario[0][4] == password:
-        return {"id": usuario[0][0], "name": usuario[0][1], "email": usuario[0][2]}
-    return None
+    if usuario and usuario[4] == password:
+        return {"id": usuario[0], "name": usuario[1], "email": usuario[2]}
+    return False
 
 def sign_usuario(name, email, matricula, password):
     run_query(f"insert into Usuarios (user_name, matricula, email, user_password) values ('{name}', '{matricula}', '{email}', '{password}');", False)
